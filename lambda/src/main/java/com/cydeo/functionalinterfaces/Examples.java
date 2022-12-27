5c718caf06b72abdd9d56762f1e48a7ffbeb7514
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2022-12-26 21:11:25 -0500

    Functional interface

--- lambda/src/main/java/com/cydeo/functionalinterfaces/Examples.java
+++ lambda/src/main/java/com/cydeo/functionalinterfaces/Examples.java
@@ -1,6 +1,6 @@
 package com.cydeo.functionalinterfaces;
 
-import java.util.function.Predicate;
+import java.util.function.*;
 
 public class Examples {
 
@@ -17,8 +17,28 @@ public class Examples {
 //        lesserThan.test(50);
 
     Predicate<Integer> lesserThan = i -> i<18;
-
         System.out.println("lesserThan.test(20) = " + lesserThan.test(20));
 
+
+        // ************ Consumer *******************//
+
+        Consumer<Integer> display = i -> System.out.println(i);
+        display.accept(100);
+
+        // ************ BiConsumer *******************//
+
+        BiConsumer<Integer,Integer> addTwo = (x,y) -> System.out.println(x+y);
+        addTwo.accept(100,200);
+
+        // ************ Function *******************//
+
+        Function<String,String> fun = s -> "Hello " +s;
+        String str = fun.apply("Java");
+        System.out.println(str);
+
+        // ************ Supplier *******************//
+
+        Supplier<Double> random = () -> Math.random();
+        System.out.println("random.get() = " + random.get());
     }
 }
